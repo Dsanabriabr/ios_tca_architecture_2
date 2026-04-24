f1c6d758c316f78b92b3e2efd911c9986b305bf3
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-04-24 18:56:50 -0300

    Initial "hello world" App project setup with Unit and UI Test

---
+++ TCA_Architecture_2/TCA_Architecture_2/TCA_Architecture_2App.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Architecture_2App.swift
+//  TCA_Architecture_2
+//
+//  Created by Daniel Sanabria on 24/04/26.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Architecture_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCA_Architecture_2/TCA_Architecture_2Tests/TCA_Architecture_2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  TCA_Architecture_2Tests.swift
+//  TCA_Architecture_2Tests
+//
+//  Created by Daniel Sanabria on 24/04/26.
+//
+
+import Testing
+
+struct TCA_Architecture_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
