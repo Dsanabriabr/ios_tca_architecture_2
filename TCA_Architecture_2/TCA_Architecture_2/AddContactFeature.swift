372d0c915c964d58e0e7e3fba2979de44bff0f7e
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-04-27 12:34:17 -0300

    DEV - Create Delegate at Action switch case, add @dependency and update body with send delegate with object - AddContactFeature.swift

--- TCA_Architecture_2/TCA_Architecture_2/AddContactFeature.swift
+++ TCA_Architecture_2/TCA_Architecture_2/AddContactFeature.swift
@@ -10,30 +10,39 @@ import SwiftUI
 
 @Reducer
 struct AddContactFeature {
-  @ObservableState
-  struct State: Equatable {
-    var contact: Contact
-  }
-  enum Action {
-    case cancelButtonTapped
-    case saveButtonTapped
-    case setName(String)
-  }
-  var body: some ReducerOf<Self> {
-    Reduce { state, action in
-      switch action {
-      case .cancelButtonTapped:
-        return .none
-        
-      case .saveButtonTapped:
-        return .none
-        
-      case let .setName(name):
-        state.contact.name = name
-        return .none
-      }
+    @ObservableState
+    struct State: Equatable {
+        var contact: Contact
     }
-  }
+    enum Action {
+        case cancelButtonTapped
+        case delegate(Delegate)
+        case saveButtonTapped
+        case setName(String)
+        enum Delegate: Equatable {
+            case saveContact(Contact)
+        }
+    }
+    @Dependency(\.dismiss) var dismiss
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .cancelButtonTapped:
+                return .run { _ in await self.dismiss()}
+                
+            case .saveButtonTapped:
+                return .run { [contact = state.contact] send in
+                  await send(.delegate(.saveContact(contact)))
+                  await self.dismiss()
+                }
+            case let .setName(name):
+                state.contact.name = name
+                return .none
+            case .delegate(_):
+                return .none
+            }
+        }
+    }
 }
 
 struct AddContactView: View {
@@ -54,7 +63,7 @@ struct AddContactView: View {
         }
     }
 }
-    
+
 #Preview {
     NavigationStack {
         AddContactView(
