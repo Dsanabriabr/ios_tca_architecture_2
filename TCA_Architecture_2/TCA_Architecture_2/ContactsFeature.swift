f2fdbdeb3c1465b1a6988f7a06798fd362cb77a5
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-04-27 17:07:12 -0300

    DEV - Navigation stacks Section3 - Add body case to path with confirmDeletaion - ContactDetailFeature.swift

--- TCA_Architecture_2/TCA_Architecture_2/ContactsFeature.swift
+++ TCA_Architecture_2/TCA_Architecture_2/ContactsFeature.swift
@@ -53,6 +53,11 @@ struct ContactsFeature {
           return .none
       case .destination(.presented(.addContact(.setName(_)))):
           return .none
+        case let .path(.element(id: id, action: .delegate(.confirmDeletion))):
+            guard let detailState = state.path[id: id]
+            else { return .none }
+            state.contacts.remove(id: detailState.contact.id)
+            return .none
         case .path:
             return .none
       }
