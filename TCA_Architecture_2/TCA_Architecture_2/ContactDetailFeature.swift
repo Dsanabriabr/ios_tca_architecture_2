0d2579e14326c2258dabd327374d2a06f2c5762c
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-04-27 16:34:14 -0300

    DEV - Navigation stacks Section1 - Add ContactDetailFeature - ContactDetailFeature.swift

---
+++ TCA_Architecture_2/TCA_Architecture_2/ContactDetailFeature.swift
@@ -0,0 +1,50 @@
+//
+//  ContactDetailFeature.swift
+//  TCA_Architecture_2
+//
+//  Created by Daniel Sanabria on 27/04/26.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+struct ContactDetailFeature {
+  @ObservableState
+  struct State: Equatable {
+    let contact: Contact
+  }
+  enum Action {
+  }
+  var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      }
+    }
+  }
+}
+
+struct ContactDetailView: View {
+  let store: StoreOf<ContactDetailFeature>
+  
+  var body: some View {
+    Form {
+    }
+    .navigationTitle(Text(store.contact.name))
+  }
+}
+
+
+#Preview {
+  NavigationStack {
+    ContactDetailView(
+      store: Store(
+        initialState: ContactDetailFeature.State(
+          contact: Contact(id: UUID(), name: "Blob")
+        )
+      ) {
+        ContactDetailFeature()
+      }
+    )
+  }
+}
