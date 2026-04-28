08c59402f4dd4e04ceb3c4c849acbe4833c4227f
Daniel Sanabria <sanabriabrasil@hotmail.com>
2026-04-28 14:41:17 -0300

    FIX - Remove redundante AlertState extension

--- TCA_Architecture_2/TCA_Architecture_2/ContactsFeature.swift
+++ TCA_Architecture_2/TCA_Architecture_2/ContactsFeature.swift
@@ -145,16 +145,3 @@ struct ContactsView: View {
         }
     )
 }
-
-// MARK: - AlertState
-extension AlertState where Action == ContactsFeature.Action.Alert {
-    static func deleteConfirmation(id: UUID) -> Self {
-        Self {
-            TextState("Are you sure?")
-        } actions: {
-            ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
-                TextState("Delete")
-            }
-        }
-    }
-}
